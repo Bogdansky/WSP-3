6220efe1dce1b867390342c041a8e124a0bb75ca
Bogdan <bogdya2@gmail.com>
2019-09-29 17:04:48 +0300

    Add main script file

---
+++ ПWS_3/Scripts/index.js
@@ -0,0 +1,36 @@
+﻿const studentsUri = "api/students/";
+
+let templates = {
+    "get": "Get",
+    "post": "Post",
+    "put": "Put",
+    "delete": "Delete",
+    "get_paginated": "GetPaginated",
+    "get_filtered": "GetFiltered",
+    "get_filtered_paginated": "GetFilteredAndPaginated"
+}
+
+const methods = Object.keys(templates).map(key => {
+    return { [key]: studentsUri + templates[key] }
+});
+
+const handlers = {
+    "get": readStudent,
+    "post": createStudent,
+    "put": updateStudent,
+    "delete": deleteStudent,
+    "get_paginated": readPagedStudents,
+    "get_filtered": readFilteredStudents,
+    "get_filtered_paginated": readFilteredPaginatedStudents
+}
+
+function onButtonClick(e) {
+    let id = e.target.id;
+
+}
+
+function onCheckBoxClick(element) {
+    let id = element.id;
+    let div = document.querySelector(`div #${id}`).firstChild;
+
+}
